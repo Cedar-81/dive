52fdf5d602ef95de1b952d94c052b8c63f87f10b
Cedar-81 <cedarwudnovels@gmail.com>
2024-08-11 06:06:05 +0100

    added error handling and switched to using csv and serde package to handle csv

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ use std::{
     io::{BufWriter, Read, Write},
 };
 
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct Record {
+    key: String,
+    value: String,
+}
+
 struct DiveStore {
     kv_store: HashMap<String, String>,
 }
@@ -15,45 +21,42 @@ impl DiveStore {
         }
     }
 
-    fn get(&self, key: String) -> Option<&String> {
-        self.kv_store.get(&key)
+    fn get(&self, key: &str) -> Option<&String> {
+        self.kv_store.get(key)
     }
 
     fn set(&mut self, key: String, value: String) -> Option<String> {
         self.kv_store.insert(key, value)
     }
 
-    fn persist(&mut self, filename: String) {
-        let file = File::create(&filename).unwrap_or_else(|err| {
-            panic!("Failed to create or open file '{}': {}", filename, err);
-        });
-
-        let mut data_file = BufWriter::new(file); //creates a file write stream
+    fn persist(&mut self, filename: String) -> Result<(), Box<dyn std::error::Error>> {
+        let mut wtr = csv::Writer::from_path(filename)?;
 
         self.kv_store.iter().for_each(|(k, v)| {
-            let value = format!("{}, {}\n", k, v);
-            data_file.write_all(value.as_bytes()).expect("Write failed");
+            let _ = wtr.serialize(Record {
+                key: k.to_string(),
+                value: v.to_string(),
+            });
         });
 
-        data_file.flush().expect("Failed to flush buffer"); //ensures all burfered content reaches their destination
+        let _ = wtr.flush();
+
+        Ok(())
     }
 
     // read from file and create new HashMap
-    fn load_store(filename: String) -> Self {
+    fn load_store(filename: String) -> Result<Self, Box<dyn std::error::Error>> {
         let mut kv_store = DiveStore {
             kv_store: HashMap::new(),
         };
-        let mut data_file = File::open(filename).unwrap();
-        let mut file_string = String::new();
+        let mut rdr = csv::Reader::from_path(filename).expect("Couldn't load file.");
 
-        let _ = data_file.read_to_string(&mut file_string);
-
-        file_string.lines().for_each(|line| {
-            let data_vec: Vec<&str> = line.split(",").collect();
-            kv_store.set(data_vec[0].to_owned(), data_vec[1].to_owned());
+        rdr.deserialize().for_each(|value| {
+            let record: Record = value.expect("File data cannot be deserialized to Record.");
+            kv_store.set(record.key, record.value);
         });
 
-        return kv_store;
+        return Ok(kv_store);
     }
 
     fn reset_inmem(&mut self) {
@@ -62,38 +65,35 @@ impl DiveStore {
 }
 
 fn main() {
-    // let mut data_file = File::open("hello.csv").unwrap_or_else(|err| {
-    //     println!("Failed to open file: {}", err);
-    //     println!("Creating new file with name: 'hello.csv'");
-    //     File::create("hello.csv").unwrap()
-    // });
-    // let mut file_string = String::new();
-
-    // let mut store = DiveStore::new();
-    // store.set("1".to_owned(), "Divine".to_owned());
-    // store.set("2".to_owned(), "Cedar".to_owned());
-    // store.set("3".to_owned(), "Grace".to_owned());
-    // store.set("4".to_owned(), "Levi".to_owned());
-
-    // println!("first value is: {}", store.get("1".to_owned()).unwrap());
-
-    // store.persist("hello.csv".to_owned());
-
     let mut store2 = DiveStore::load_store("hello.csv".to_owned());
 
-    println!(
-        "first value is for store2: {}",
-        store2.get("1".to_owned()).unwrap()
-    );
+    match store2 {
+        Ok(mut store) => {
+            println!(
+                "first value is for store2: {}",
+                store
+                    .get("1")
+                    .unwrap_or(&"Couldn't find key in store".to_string())
+            );
 
-    store2.set("5".to_owned(), "Yin".to_owned());
-    store2.set("6".to_owned(), "Yang".to_owned());
+            store.set("5".to_owned(), "Yin".to_owned());
+            store.set("6".to_owned(), "Yang".to_owned());
+            store.set("7".to_owned(), "Me".to_owned());
 
-    println!(
-        "first and sixth values for store2 is: {}, {}",
-        store2.get("1".to_owned()).unwrap(),
-        store2.get("6".to_owned()).unwrap(),
-    );
+            println!(
+                "first and sixth values for store2 is: {}, {}",
+                store
+                    .get("1")
+                    .unwrap_or(&"Couldn't find key in store".to_string()),
+                store
+                    .get("6")
+                    .unwrap_or(&"Couldn't find key in store".to_string())
+            );
 
-    store2.persist("hello.csv".to_owned());
+            let _ = store.persist("hello.csv".to_owned());
+            // let mut store = DiveStore::new();
+            // let _ = store.persist("hello.csv".to_owned());
+        }
+        Err(e) => eprint!("An error occurred while attempting to load store: {} ", e),
+    }
 }
